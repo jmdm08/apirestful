a12f1320ceae43ed1c75f527c515fb18062257de
José Miguel Dager Montoya <jose.dager@gmail.com>
2021-11-02 07:59:02 -0500

    feature(): Botón JSON

--- public/js/login.js
+++ public/js/login.js
@@ -5,6 +5,50 @@ window.onload = function(){
 function iniciarEventos(){
     document.getElementById("btnIniciarSesion").addEventListener("click", iniciarSesion);
     document.getElementById("btnRegistrar").addEventListener("click", crearUsuario);
+    document.getElementById("btnJSON").addEventListener("click", mostrarJSON);
+}
+
+function mostrarJSON(evento){
+    // JSON
+    let json = [
+        {
+            "nombres": "José",
+            "apellidos": "Dager",
+            "telefonos": [123, 456, 789],
+            "direccion": {
+                "barrio": "Montería",
+                "ciudad": "Montería",
+                "ruta": "XXX # 10-20 XXX YYY"
+            }
+        },
+        {
+            "nombres": "Isabella",
+            "apellidos": "Dager",
+            "telefonos": [321, 654],
+            "edad": 88,
+            "fechaNacimiento": "2021-11-02",
+            "direccion": {}
+        }
+    ]
+    let jsonString = JSON.stringify(json);
+    console.log(jsonString);
+    console.log(json);
+    // EXTRAER DATOS.
+    console.log(json[1].nombres);
+    console.log(json[0].direccion.ciudad);
+    console.log(json[1].telefonos[1]);
+
+    // ADICIONAR / MODIFICAR
+    json[0].edad = 15;
+    json[0].fechaNacimiento = "xxxx-10-20";
+    json[1].direccion = {
+        "barrio": "XXXX",
+        "ciudad": "YYYY",
+        "ruta": "xxx # 10-20 TYYY YYY"
+    }
+
+    console.log(json);
+
 }
 
 function crearUsuario(evento){
